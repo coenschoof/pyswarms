bcab7c1f85cc40318fd360d675fea4710e64622e
Coen Schoof <coenschoof@gmail.com>
2022-06-09 17:48:37 +0200

    .

--- pyswarms/single/global_best.py
+++ pyswarms/single/global_best.py
@@ -212,7 +212,8 @@ class GlobalBestPSO(SwarmOptimizer):
 
         while datetime.datetime.now() <= endTime:
             for i in self.rep.pbar(iters, self.name) if verbose else range(iters):
-                print(' ', datetime.datetime.now(), endTime)
+                #print(' ', datetime.datetime.now(), endTime)
+                print('test')
                 #self.rep.log(datetime.datetime.now(), lvl=log_level)
                 #self.rep.log(endTime, lvl=log_level)
                 # Compute cost for current position and personal best
